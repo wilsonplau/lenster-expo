183879525add8a5ed87a067fb6b5953b0bab4493
Wilson Lau <hello@wilsonplau.com>
2023-05-17 21:03:39 -0400

    feat: setup nativewind

---
+++ components/base.ts
@@ -0,0 +1,5 @@
+import { Text as ReactNativeText, View as ReactNativeView } from "react-native";
+import { styled } from "nativewind";
+
+export const View = styled(ReactNativeView);
+export const Text = styled(ReactNativeText);
